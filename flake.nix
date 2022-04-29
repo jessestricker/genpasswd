d2122ac40bfbace77d7af1c3ee079e95cd6d2a3b
Jesse Stricker <me@jessestricker.de>
2022-04-30 00:09:07 +0200

    Add named flake outputs

--- flake.nix
+++ flake.nix
@@ -16,15 +16,20 @@
         pkgs = nixpkgs.legacyPackages."${system}";
         naersk-lib = naersk.lib."${system}";
       in rec {
-        packages.default = naersk-lib.buildPackage {
+        # nix build
+        packages.genpasswd = naersk-lib.buildPackage {
           pname = "genpasswd";
           root = ./.;
         };
+        packages.default = packages.genpasswd;
 
-        apps.default = flake-utils.lib.mkApp {
-          drv = packages.default;
+        # nix run
+        apps.genpasswd = flake-utils.lib.mkApp {
+          drv = packages.genpasswd;
         };
+        apps.default = apps.genpasswd;
 
+        # nix develop
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [cargo rustfmt clippy];
         };
