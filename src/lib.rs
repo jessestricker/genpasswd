c655efb0969dcc3ce80af4a9efe2b12335a49562
Jesse Stricker <me@jessestricker.de>
2022-04-30 18:14:35 +0200

    Add feature to select char set

---
+++ src/lib.rs
@@ -0,0 +1,85 @@
+use std::collections::BTreeSet;
+
+/// An ordered set of unique characters.
+pub type CharSet = BTreeSet<char>;
+
+pub mod chars {
+    use super::CharSet;
+    use lazy_static::lazy_static;
+    use std::ops::RangeInclusive;
+
+    const GRAPHIC: RangeInclusive<char> = '\x21'..='\x7E';
+
+    lazy_static! {
+        /// The set of uppercase letters in the _Basic Latin_ block.
+        ///
+        /// Generated from `[[:blk=ASCII:]&[:gc=Lu:]]`.
+        pub static ref LETTERS_UPPER: CharSet = ('A'..='Z').collect();
+
+        /// The set of lowercase letters in the _Basic Latin_ block.
+        ///
+        /// Generated from `[[:blk=ASCII:]&[:gc=Ll:]]`.
+        pub static ref LETTERS_LOWER: CharSet = ('a'..='z').collect();
+
+        /// The set of decimal digits in the _Basic Latin_ block.
+        ///
+        /// Generated from `[[:blk=ASCII:]&[:gc=Nd:]]`.
+        pub static ref DIGITS: CharSet = ('0'..='9').collect();
+
+        /// The set of symbols and punctuation in the _Basic Latin_ block.
+        ///
+        /// Generated from `[[:blk=ASCII:]&[[:gc=S:]|[:gc=P:]]]`.
+        pub static ref SYMBOLS: CharSet = GRAPHIC.filter(char::is_ascii_punctuation).collect();
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn all_sets_are_disjoint() {
+            assert!(LETTERS_UPPER.is_disjoint(&LETTERS_LOWER));
+            assert!(LETTERS_UPPER.is_disjoint(&DIGITS));
+            assert!(LETTERS_UPPER.is_disjoint(&SYMBOLS));
+
+            assert!(LETTERS_LOWER.is_disjoint(&DIGITS));
+            assert!(LETTERS_LOWER.is_disjoint(&SYMBOLS));
+
+            assert!(DIGITS.is_disjoint(&SYMBOLS));
+        }
+
+        #[test]
+        fn all_sets_unite_to_graphics() {
+            let all_sets: CharSet = (LETTERS_UPPER.iter())
+                .chain(LETTERS_LOWER.iter())
+                .chain(DIGITS.iter())
+                .chain(SYMBOLS.iter())
+                .cloned()
+                .collect();
+            let graphic_set: CharSet = GRAPHIC.collect();
+            assert_eq!(graphic_set, all_sets);
+        }
+    }
+}
+
+/// The properties from which a password string is generated.
+pub struct Password<'a> {
+    /// The set of characters to randomly choose from.
+    pub char_set: &'a CharSet,
+
+    /// The character length of the password.
+    pub length: u16,
+}
+
+impl<'a> Password<'a> {
+    /// Generates a random password string.
+    pub fn generate(&self) -> String {
+        use rand::prelude::*;
+
+        let mut rng = StdRng::from_entropy();
+
+        (0..self.length)
+            .map(|_| self.char_set.iter().choose(&mut rng).unwrap())
+            .collect()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,52 @@
-use clap::Parser;
-use rand::prelude::*;
-use std::ops::RangeInclusive;
-
-struct Password {
-    length: u16,
-}
-
-impl Password {
-    const CHAR_SET: RangeInclusive<char> = '\u{0021}'..='\u{007E}';
-
-    fn generate(&self) -> String {
-        let mut rng = StdRng::from_entropy();
-        (0..self.length)
-            .map(|_| Self::CHAR_SET.choose(&mut rng).unwrap())
-            .collect()
-    }
-}
+use clap::{ArgEnum, Parser};
+use genpasswd::{chars, CharSet, Password};
+use lazy_static::lazy_static;
 
 #[derive(Parser)]
 #[clap(version, author, about)]
 struct Args {
     /// The length of the generated password.
     #[clap(short = 'n', long)]
-    length: u16,
+    pub length: u16,
+
+    /// The set of characters to choose from.
+    #[clap(short, long, arg_enum, default_value_t = Type::Ascii)]
+    pub r#type: Type,
+}
+
+#[derive(ArgEnum, Clone)]
+enum Type {
+    Letters,
+    Digits,
+    LettersDigits,
+    Ascii,
+}
+
+lazy_static! {
+    static ref LETTERS: CharSet = &*chars::LETTERS_UPPER | &*chars::LETTERS_LOWER;
+    static ref LETTERS_AND_DIGITS: CharSet = &*LETTERS | &*chars::DIGITS;
+    static ref ASCII: CharSet = &*LETTERS_AND_DIGITS | &*chars::SYMBOLS;
+}
+
+impl Type {
+    fn char_set(&self) -> &'static CharSet {
+        match self {
+            Type::Letters => &LETTERS,
+            Type::Digits => &chars::DIGITS,
+            Type::LettersDigits => &LETTERS_AND_DIGITS,
+            Type::Ascii => &ASCII,
+        }
+    }
 }
 
 fn main() {
     let args = Args::parse();
+
     let passwd = Password {
+        char_set: args.r#type.char_set(),
         length: args.length,
     };
+
     let passwd_str = passwd.generate();
     println!("{}", passwd_str);
 }
